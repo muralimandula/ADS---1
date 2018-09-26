51e011771c54acbe7ebc3462712620d0ab8c81ca
muralimandula <murali.mandula@msitprogram.net>
2018-09-26 23:07:44 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ ads1-assignments/m3/3-sum/Solution.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+
+
+public class Solution {
+
+
+  private Solution() {
+    // unused constructor
+  }
+
+  /**
+   * Main function.
+   * Returns the combinational number of equal pairs in the given array.
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    int noOfInputs = scan.nextInt();
+
+    ArrayList<Integer> repeatCount = new ArrayList<>();
+
+    int[] array = new int[noOfInputs];
+    							//An int[] create for given size.
+
+    for (int i = 0; i < noOfInputs; i++) {
+      array[i] = scan.nextInt();
+    }   						//Values inserted into int[].
+
+
+    Arrays.sort(array);
+
+    int count = 0;
+
+    for(int initial = 0; initial < noOfInputs; initial++) {
+
+    	int last = noOfInputs;
+
+    	int i = 0;
+
+    	int initialElement = array[initial];
+
+    	while ( i != initial && last != initial) {
+
+    		int threeSum = array[i] + array[last];
+
+    		if (threeSum + initialElement > 0) {
+    			last -= 1;
+
+    		} else if (threeSum + initialElement < 0) {
+    			i += 1;
+
+    		} else {
+    			count += 1;
+    			i++;
+    		}
+    	}
+
+    }
+
+    System.out.println(count);
+
+
+   }
+}
