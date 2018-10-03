9f171ba6e0643b7193351c02635afb3e0a286dca
muralimandula <murali.mandula@msitprogram.net>
2018-10-03 20:32:43 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ads1-assignments/m8/8.2 Team Ranking With Selection/Solution.java
+++ ads1-assignments/m8/8.2 Team Ranking With Selection/Solution.java
@@ -1,48 +1,68 @@
 import java.util.Scanner;
 
+/**
+ * Solution class.
+ */
 public final class Solution {
 
-	Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
+    }
 
-	public static String leaderBoard(final Team[] teams, final int size) {
+    /**
+     * Leaderboard format of teams.
+     *
+     * @param      teams  The teams
+     * @param      size   The size
+     *
+     * @return     String format of leaderboard.
+     */
+    public static String leaderBoard(final Team[] teams, final int size) {
 
-	    SelectionSortTeam.sort(teams, size);
+        SelectionSortTeam.sort(teams, size);
 
-	    String leaderboard = "";
-	    for (int i = 0; i < size; i++) {
-	      leaderboard += teams[i].toString();
-	      leaderboard += ",";
-	    }
-	    leaderboard = leaderboard.substring(0, leaderboard.length() - 1);
-	    return leaderboard;
-	  }
-
-
+        String leaderboard = "";
+        for (int i = 0; i < size; i++) {
+          leaderboard += teams[i].toString();
+          leaderboard += ",";
+        }
+        leaderboard = leaderboard.substring(0, leaderboard.length() - 1);
+        return leaderboard;
+      }
 
 
-	public static void main(String[] args) {
+
+    /**
+     * Main fUnction.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
 
 
-   		final int three = 3;
-   		final int ten = 10;
-   		int size = 0;
+        final int three = 3;
+        final int ten = 10;
+        int size = 0;
 
-		Team[] teams = new Team[ten];
+        Team[] teams = new Team[ten];
 
-		Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-		while(scan.hasNext()) {
+        while (scan.hasNext()) {
 
-			String[] teamRecord = scan.nextLine().split(",");
+            String[] teamRecord = scan.nextLine().split(",");
 
-			Team temp = new Team(teamRecord[0], Integer.parseInt(teamRecord[1]),
-                      Integer.parseInt(teamRecord[2]), Integer.parseInt(teamRecord[three]));
-			teams[size] = temp;
-			size++;
-		}
-		scan.close(); //closing scanner.
-		System.out.println(leaderBoard(teams, size));
-	}
+            Team temp = new Team(teamRecord[0],
+                     Integer.parseInt(teamRecord[1]),
+                          Integer.parseInt(teamRecord[2]),
+                           Integer.parseInt(teamRecord[three]));
+            teams[size] = temp;
+            size++;
+        }
+        scan.close(); //closing scanner.
+        System.out.println(leaderBoard(teams, size));
+    }
 }
