7665a084df07114f37306a03349c6de3de950173
muralimandula <murali.mandula@msitprogram.net>
2018-10-03 20:43:29 +0530

    "check_style_score -> 6 of 6 passed. style score: 1/1 "

--- ads1-assignments/m8/8.2 Team Ranking With Selection/Team.java
+++ ads1-assignments/m8/8.2 Team Ranking With Selection/Team.java
@@ -1,5 +1,7 @@
 
-
+/**
+ * Class for team.
+ */
 public class Team implements Comparable<Team> {
 
 	/**
@@ -36,6 +38,18 @@ public class Team implements Comparable<Team> {
     this.draws = noOfdraws;
   }
 
+
+  	/**
+  	 * That compares team1 with team2.
+  	 *
+  	 * This method does not have iterations, once executed will return.
+  	 *
+  	 * Hence, Complexity will be O(1).
+  	 *
+  	 * @param      team2  The team 2
+  	 *
+  	 * @return     1 - greater, -1 - lesser, 0 - equal.
+  	 */
   	public int compareTo(Team team2) {
   		if (this.wins > team2.wins) {
   			return 1;
@@ -62,6 +76,11 @@ public class Team implements Comparable<Team> {
   	}
 
 
+  	/**
+  	 * Returns a string representation of the object.
+  	 *
+  	 * @return     String representation of the object.
+  	 */
   	public String toString() {
   		return teamName;
   	}
@@ -69,8 +88,25 @@ public class Team implements Comparable<Team> {
 
 }
 
+
+/**
+ * Class for selection sort team.
+ */
 class SelectionSortTeam {
 
+	/**
+	 * Sorting the teams, through N(size/no.of.teams) teams.
+	 *
+	 * In any Case, it needs N iterations for each position.
+	 * for each team to swap at each position, another N iterations.
+	 *
+	 * Hence, Complexity Base Case : N^2.
+	 * 					 Average Case : N^2.
+	 * 					 Worst Case : N^2.
+	 *
+	 * @param      teams  The teams
+	 * @param      size   The size
+	 */
 	static void sort(Comparable[] teams, int size) {
 
 		for(int i = 0; i < size; i++) {
@@ -91,7 +127,13 @@ class SelectionSortTeam {
 		}
 	}
 
-
+	/**
+	 * Swaps two compared teams.
+	 *
+	 * @param      teams            The teams
+	 * @param      i                Index posiion to swap.
+	 * @param      minimumPosition  The minimum position
+	 */
 	static void swap(Comparable[] teams, int i, int minimumPosition) {
 
 		Comparable temp = teams[i];
