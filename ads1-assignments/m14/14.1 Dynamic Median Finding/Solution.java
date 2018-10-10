0ef10e724ab11c731e5ec0cb58e0ef08c6078940
muralimandula <murali.mandula@msitprogram.net>
2018-10-10 18:44:01 +0530

    "ads1_m14_a1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ads1-assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -1,50 +1,70 @@
-import java.util.*;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
+/**
+ * Class for solution.
+ */
 public class Solution {
 
-	Solution() {
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused.
+    }
+
+        /**
+         * Median calculation.
+         *
+         * @param      intArray  The int array
+         */
+        static void median(final ArrayList<Integer> intArray) {
+
+            // System.out.println(intArray);
+
+            Collections.sort(intArray);
+            // System.out.println(intArray);
+
+            int arraySize = intArray.size();
+            double median;
+            if (arraySize % 2 != 0) {
+
+                // System.out.println(intArray.get(arraySize / 2));
+                median = intArray.get(arraySize / 2);
+                System.out.println(median);
+            } else {
 
 
-		static void median(final ArrayList<Integer> intArray) {
+                // System.out.println((intArray.get(arraySize / 2
+                // ) + " "  + intArray.get((arraySize / 2) - 1)));
 
-			// System.out.println(intArray);
-
-			Collections.sort(intArray);
-			// System.out.println(intArray);
-
-			int arraySize = intArray.size();
-			double median;
-			if(arraySize % 2 != 0) {
-
-				// System.out.println(intArray.get(arraySize / 2));
-				median = intArray.get(arraySize / 2);
-				System.out.println(median);
-			} else {
-
-
-				// System.out.println((intArray.get(arraySize / 2) + " "  + intArray.get((arraySize / 2) - 1)));
-
-				median = ((double)intArray.get(arraySize / 2) + intArray.get((arraySize / 2) - 1)) / 2;
+                median = ((double) intArray.get(arraySize / 2)
+                 + intArray.get((arraySize / 2) - 1)) / 2;
 
 
 
-				System.out.println(median);
+                System.out.println(median);
 
-			}
+            }
 
-		}
+        }
 
-		public static void main(final String[] args) {
+        /**
+         * Main Method.
+         *
+         * @param      args  The arguments
+         */
+        public static void main(final String[] args) {
 
-			Scanner scan = new Scanner(System.in);
+            Scanner scan = new Scanner(System.in);
 
-			int nOfIntegers = Integer.parseInt(scan.nextLine());
-			ArrayList<Integer> array = new ArrayList<>();
-			for (int i = 0; i < nOfIntegers; i++) {
-				array.add(Integer.parseInt(scan.nextLine()));
-				median(array);
-			}
+            int nOfIntegers = Integer.parseInt(scan.nextLine());
+            ArrayList<Integer> array = new ArrayList<>();
+            for (int i = 0; i < nOfIntegers; i++) {
+                array.add(Integer.parseInt(scan.nextLine()));
+                median(array);
+            }
 
-		}
+        }
 }
