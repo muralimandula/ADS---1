6876280f8985d02d8144f99138acfc7a8d2fd824
muralimandula <murali.mandula@msitprogram.net>
2018-10-12 19:58:30 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m17/TaxiCabNumbers/Solution.java
+++ ads1-assignments/m17/TaxiCabNumbers/Solution.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 
 
@@ -7,41 +7,61 @@ import java.util.*;
  */
 public class Solution {
 
-	/**
-	 * Main Method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
-		for (int i = 1; i <= 1000; i++) {
-			pq.insert(new CubeSum(i, i));
-		}
-		CubeSum prev = new CubeSum(0, 0);
-		int sum = 0;
-		int p = 1;
-		int k = 0;
-		while (!pq.isEmpty()) {
-			CubeSum c = pq.delMin();
-			if (prev.sum() == c.sum()) {
-				p++;
-				if (p == m) {
-					sum = c.sum();
+    /**
+     * Magic number.
+     */
+    private static final int a = 1000;
 
-					if (++k == n) break;
-				}
-			} else {
-				p = 1;
-			}
-			prev = c;
-			if (c.j < 1500)
-				pq.insert(new CubeSum(c.i, c.j + 1));
+    /**
+     * Magic Number.
+     */
+    private static final int b = 1500;
 
-		}
-		System.out.println(sum);
+    /**
+     * Constructs the object.
+     */
+    Solution() {
+        //unused.
+    }
 
-	}
+    /**
+     * Main Method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        MinPQ<CubeSum> pq = new MinPQ<CubeSum>();
+        for (int i = 1; i <= a; i++) {
+            pq.insert(new CubeSum(i, i));
+        }
+        CubeSum prev = new CubeSum(0, 0);
+        int sum = 0;
+        int p = 1;
+        int k = 0;
+        while (!pq.isEmpty()) {
+            CubeSum c = pq.delMin();
+            if (prev.sum() == c.sum()) {
+                p++;
+                if (p == m) {
+                    sum = c.sum();
+
+                    if (++k == n) {
+                        break;
+                    }
+                }
+            } else {
+                p = 1;
+            }
+            prev = c;
+            if (c.j < b) {
+                pq.insert(new CubeSum(c.i, c.j + 1));
+            }
+
+        }
+        System.out.println(sum);
+
+    }
 }
