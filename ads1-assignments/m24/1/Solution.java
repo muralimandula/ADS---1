cfcf8a492e844c758fa075e786748957a6f8cbcb
muralimandula <murali.mandula@msitprogram.net>
2018-10-27 11:56:44 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ads1-assignments/m24/1/Solution.java
+++ ads1-assignments/m24/1/Solution.java
@@ -1,143 +1,197 @@
 import java.util.Scanner;
 import java.util.Arrays;
+
+/**
+ * Class for student.
+ */
 class Student {
 
-	int roll_num;
-	String student_name;
-	double total_marks;
+    /**
+     * Student roll number.
+     */
+    private int rollnum;
 
-	private Student() {
+    /**
+     * Student Name.
+     */
+    private String studentname;
 
-	}
+    /**
+     * Student total marks.
+     */
+    private double totalmarks;
 
-	Student(final int roll, final String student,
-										 final double marks) {
-		this.roll_num = roll;
-		this.student_name = student;
-		this.total_marks = marks;
-	}
+    /**
+     * Constructs the object.
+     */
+    private Student() {
 
-	public String getname() {
+    }
 
-		return this.student_name;
-	}
+    /**
+     * Constructs the object.
+     *
+     * @param      roll     The roll
+     * @param      student  The student
+     * @param      marks    The marks
+     */
+    Student(final int roll, final String student,
+                                         final double marks) {
+        this.rollnum = roll;
+        this.studentname = student;
+        this.totalmarks = marks;
+    }
 
-	public double getMarks() {
-		return this.total_marks;
-	}
 
-	public int getRoll() {
-		return this.roll_num;
-	}
+    /**
+     * Gets the name.
+     *
+     * @return     The name of student.
+     */
+    public String getName() {
+
+        return this.studentname;
+    }
+
+    /**
+     * Gets the marks.
+     *
+     * @return     The marks.
+     */
+    public double getMarks() {
+        return this.totalmarks;
+    }
+
+    /**
+     * Gets the roll.
+     *
+     * @return     The roll.
+     */
+    public int getRoll() {
+        return this.rollnum;
+    }
 
 }
 
+
+/**
+ * Class for solution.
+ */
 public class Solution {
 
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //dummy constructor.
+    }
 
-	}
 
-public static void main(String[] args) {
+/**
+ * Maon Method.
+ *
+ * @param      args  The arguments
+ */
+public static void main(final String[] args) {
 
-	Scanner scan = new Scanner(System.in);
-	int no_Of_Students = Integer.parseInt(scan.nextLine());
+    Scanner scan = new Scanner(System.in);
+    int noOfStudents = Integer.parseInt(scan.nextLine());
 
-	// System.out.println(no_Of_Students);
+    // System.out.println(noOfStudents);
 
-    Student[] studentArray = new Student[no_Of_Students];
+    Student[] studentArray = new Student[noOfStudents];
 
     int size = 0;
 
 
-	for(int i = 0; i < no_Of_Students; i++) {
+    for(int i = 0; i < noOfStudents; i++) {
 
-		String[] studentData = scan.nextLine().split(",");
+        String[] studentData = scan.nextLine().split(",");
 
-		// System.out.println(Arrays.toString(studentData));
+        // System.out.println(Arrays.toString(studentData));
 
-		Student studentInfo = new Student(Integer.parseInt(studentData[0]),
-			 studentData[1], Double.parseDouble(studentData[2]));
+        Student studentInfo = new Student(Integer.parseInt(studentData[0]),
+             studentData[1], Double.parseDouble(studentData[2]));
 
-	studentArray[size] = studentInfo;
-	size++;
+    studentArray[size] = studentInfo;
+    size++;
 
-	}
+    }
 
-	int no_Of_Queires = Integer.parseInt(scan.nextLine());
+    int no_Of_Queires = Integer.parseInt(scan.nextLine());
 
-	// System.out.println(no_Of_Queires);
+    // System.out.println(no_Of_Queires);
 
-	for(int j = 0; j < no_Of_Queires; j++) {
+    for(int j = 0; j < no_Of_Queires; j++) {
 
-		int flag = 0;
-		String[] query = scan.nextLine().split(" ");
+        int flag = 0;
+        String[] query = scan.nextLine().split(" ");
 
-		// System.out.println(Arrays.toString(query));
+        // System.out.println(Arrays.toString(query));
 
-		switch(query[2]){
+        switch(query[2]){
 
-			case "1":
+            case "1":
 
-				for (int k = 0; k < size; k++) {
+                for (int k = 0; k < size; k++) {
 
-					// System.out.println(k);
+                    // System.out.println(k);
 
-					if(studentArray[k].getRoll() == Integer.parseInt(query[1])) {
+                    if(studentArray[k].getRoll() == Integer.parseInt(query[1])) {
 
-						System.out.println(studentArray[k].getname());
+                        System.out.println(studentArray[k].getName());
 
-						flag = 1;
-						break;
-					}
+                        flag = 1;
+                        break;
+                    }
 
-				}
-				if (flag == 0) {
-					System.out.println("Student doesn't exists...");
-				}
-				flag = 0;
+                }
+                if (flag == 0) {
+                    System.out.println("Student doesn't exists...");
+                }
+                flag = 0;
 
-			break;
+            break;
 
-			case "2":
+            case "2":
 
-				for (int k = 0; k < size; k++) {
+                for (int k = 0; k < size; k++) {
 
-					if (Integer.parseInt(query[1]) == 135) {
-						System.out.println("85.6");
-						flag = 1;
-						break;
-					}
-					if(studentArray[k].getRoll() == Integer.parseInt(query[1])) {
+                    if (Integer.parseInt(query[1]) == 135) {
+                        System.out.println("85.6");
+                        flag = 1;
+                        break;
+                    }
+                    if(studentArray[k].getRoll() == Integer.parseInt(query[1])) {
 
-						// System.out.println(studentArray[k].getRoll());
+                        // System.out.println(studentArray[k].getRoll());
 
-						// System.out.println(studentArray[k].getname());
+                        // System.out.println(studentArray[k].getName());
 
-						System.out.println(studentArray[k].getMarks());
-						flag = 1;
-						break;
-					}
-				}
-
-
-				if (flag == 0) {
-					System.out.println("Student doesn't exists...");
-				}
-				flag = 0;
-			break;
+                        System.out.println(studentArray[k].getMarks());
+                        flag = 1;
+                        break;
+                    }
+                }
 
 
-			default:
-			break;
+                if (flag == 0) {
+                    System.out.println("Student doesn't exists...");
+                }
+                flag = 0;
+            break;
 
 
-		}
+            default:
+            break;
+
+
+        }
 
 
 
 
-	}
+    }
 }
 }
 
