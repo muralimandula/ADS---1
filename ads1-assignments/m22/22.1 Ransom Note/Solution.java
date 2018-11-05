0ed6828adf20e81062ea6d7ea61302c63434b0a5
muralimandula <murali.mandula@msitprogram.net>
2018-11-05 12:24:07 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ads1-assignments/m22/22.1 Ransom Note/Solution.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //dummy constructor.
+    }
+
+
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        //Search tree of String, integer. #hash
+        SeparateChainingHashST<String, Integer> hash
+        = new SeparateChainingHashST();
+        int k = scan.nextInt();
+        int l = scan.nextInt();
+        scan.nextLine();
+        String[] magazine = scan.nextLine().split(" ");
+        for (int i = 0; i < magazine.length; i++) {
+            if (hash.contains(magazine[i])) {
+                hash.put(magazine[i], hash.get(magazine[i]) + 1);
+            } else {
+                hash.put(magazine[i], 1);
+            }
+        }
+        String[] tobe = scan.nextLine().split(" ");
+        for (int i = 0; i < tobe.length; i++) {
+            if (hash.contains(tobe[i])) {
+                if (hash.get(tobe[i]) == 0) {
+                    System.out.print("No");
+                    return;
+                } else {
+                    hash.put(tobe[i], hash.get(tobe[i]) - 1);
+                }
+            } else {
+                System.out.print("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+}
